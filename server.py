02b60b5d525b1b88a1e2d389457ee1314132f56d
brigun <brigun@gmail.com>
2026-07-12 23:25:43 -0400

    Updated for error handling and PyLint suggestions

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Flask server application for executing emotion detection analysis
+using the embedded Watson NLP Emotion Detection package.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
 
@@ -5,33 +9,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
-    
+    """
+    Renders the main application UI.
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emot_analyzer():
-    
+    """
+    Analyzes the query string text parameters for emotions and returns
+    a formatted summary of the response.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    
-    if not text_to_analyze:
-        return "Invalid text! Please try again."
-    
     response = emotion_detector(text_to_analyze)
-    
+    dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
-    
-    output_string = (
-        "For the given statement, the system response is "
-        "'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. "
-        "The dominant emotion is {}."
-    ).format(anger, disgust, fear, joy, sadness, dominant_emotion)
-    
-    return output_string
+
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+        f"'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
